c9c0fa6ec069126595546cd83a71aa4593eb0b3b
Maya <49682339+MayaFireFly@users.noreply.github.com>
2019-06-27 18:37:45 +0500

    Create fragment.php

---
+++ fragment.php
@@ -0,0 +1,13 @@
+<?php
+
+'components' => [
+  'formatter' => [
+            'dateFormat' => 'dd.MM.yyyy',
+            'decimalSeparator' => ',',
+            'thousandSeparator' => ' ',
+            'currencyCode' => 'RUR',
+            'nullDisplay' => '<span class="not-set">нет данных</span>', //<-- нет данных instead (not set)
+       ],
+],
+
+?>
